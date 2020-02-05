177aa092afa829726ca57767ac8610c798332117
OlgaHa <gella4ka@inbox.lv>
2020-02-05 16:40:15 +0200

    inter

---
+++ ArrIntersect.java
@@ -0,0 +1,36 @@
+package bootcamp;
+
+import java.util.Arrays;
+
+public class ArrIntersect {
+
+	public static void main(String[] args) {
+		int[] list1 = { 1, 4, 8, 9, 15, 11, 17, 59, 41, 28 };
+		int[] list2 = { 4, 7, 11, 17, 19, 20, 23, 28, 37, 59, 81 };
+
+		Arrays.parallelSort(list1);
+		Arrays.parallelSort(list2);
+		System.out.println(Arrays.toString(list1));
+		System.out.println(Arrays.toString(list2));
+		System.out.print("Intersect: ");
+		intersect(list1, list2);
+
+	}
+
+	public static void intersect(int[] arr1, int[] arr2) {
+		int match = 0;
+		for (int i = 0; i < arr1.length; i++) {
+			Arrays.binarySearch(arr1, i);
+			for (int j = 0; j < arr2.length; j++) {
+				Arrays.binarySearch(arr2, j);
+				if (arr1[i] == arr2[j]) {
+					match = arr1[i];
+					System.out.print(match + ", ");
+				}
+			}
+
+		}
+
+	}
+
+}
